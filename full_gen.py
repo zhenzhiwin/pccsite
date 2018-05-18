ee31b87de37756dea1db797779ba12ede7275e95
zhenzhiwin <393452750@qq.com>
2018-05-18 17:51:49 +0800

    修改了逻辑错误

--- full_gen.py
+++ full_gen.py
@@ -1,50 +1,48 @@
-#coding=utf-8
+# coding=utf-8
 """
 @Author:zhi.zhen
 email:zhi.zhen@nokia.com
 """
 
-def gen_begining(configlist,path):
-    cmdlist=[]
+
+def gen_begining(configlist, path):
+    cmdlist = []
+    flag = True
     for line in configlist:
-        if line.find("primary 10/2\n")==-1:
-            cmdlist.append("configure isa application-assurance-group 1 aa-sub-scale mobile-gateway create\n")
-            cmdlist.append("primary 1/2\n")
-            cmdlist.append("primary 2/2\n")
-            cmdlist.append("primary 3/2\n")
-            cmdlist.append("primary 4/2\n")
-            cmdlist.append("primary 5/2\n")
-            cmdlist.append("primary 6/2\n")
-            cmdlist.append("primary 7/2\n")
-            cmdlist.append("primary 8/2\n")
-            cmdlist.append("primary 9/2\n")
-            cmdlist.append("primary 10/2\n")
-            cmdlist.append("shared-resources\n")
-            cmdlist.append("tcp-adv-func 0\n")
-            cmdlist.append("exit\n")
-            cmdlist.append("partitions\n")
-            cmdlist.append("statistics\n")
-            cmdlist.append("performance\n")
-            cmdlist.append("collect-stats\n")
-            cmdlist.append("exit\n")
-            cmdlist.append("exit\n")
-            cmdlist.append("no shutdown\n")
-            cmdlist.append("exit all\n")
+        if line.find("primary 10/2") != -1:
+            flag = False
+            break
+    if flag:
+        cmdlist.append("configure isa application-assurance-group 1 aa-sub-scale mobile-gateway create\n")
+        cmdlist.append("primary 1/2\n")
+        cmdlist.append("primary 2/2\n")
+        cmdlist.append("primary 3/2\n")
+        cmdlist.append("primary 4/2\n")
+        cmdlist.append("primary 5/2\n")
+        cmdlist.append("primary 6/2\n")
+        cmdlist.append("primary 7/2\n")
+        cmdlist.append("primary 8/2\n")
+        cmdlist.append("primary 9/2\n")
+        cmdlist.append("primary 10/2\n")
+        cmdlist.append("shared-resources\n")
+        cmdlist.append("tcp-adv-func 0\n")
+        cmdlist.append("exit\n")
+        cmdlist.append("partitions\n")
+        cmdlist.append("statistics\n")
+        cmdlist.append("performance\n")
+        cmdlist.append("collect-stats\n")
+        cmdlist.append("exit\n")
+        cmdlist.append("exit\n")
+        cmdlist.append("no shutdown\n")
+        cmdlist.append("exit all\n")
 
-            cmdlist.append("/configure application-assurance group 1:1 policy begin\n")
-            cmdlist.append('/configure application-assurance group 1:1 policy app-profile "default-app-profile" create description "Global App-profile"\n')
-            cmdlist.append('/configure application-assurance group 1:1 policy app-profile "default-app-profile" divert\n')
-            cmdlist.append('/configure application-assurance group 1:1 policy commit\n')
-            cmdlist.append('exit all\n')
+        cmdlist.append("/configure application-assurance group 1:1 policy begin\n")
+        cmdlist.append(
+            '/configure application-assurance group 1:1 policy app-profile "default-app-profile" create description "Global App-profile"\n')
+        cmdlist.append('/configure application-assurance group 1:1 policy app-profile "default-app-profile" divert\n')
+        cmdlist.append('/configure application-assurance group 1:1 policy commit\n')
+        cmdlist.append('exit all\n')
 
-    file = open(path + "\\AA Partitions.txt", "w")
-    file.writelines(cmdlist)
-    file.close()
-
-
-
-
-
-
-
-
+        file = open(path + "\\AA Partitions.txt", "w")
+        file.writelines(cmdlist)
+        file.close()
