b297a2d9300eb9d713bed56160a855ad11de9d57
tianxintong123456 <751723342@qq.com>
2018-06-05 16:26:18 +0800

    修复彩信2

---
+++ ChargingContextAai.py
@@ -0,0 +1,52 @@
+
+
+
+
+def processUrl(url):
+    e_host = None
+    e_url = None
+    h_port = None
+
+    url = url.replace("http://", "").replace("https://", "")
+    if "/*"  in url:
+        url = url.replace("/*","")
+        e_url = "/*"
+        e_host = url
+        if "/" in url:
+            e_url = url[url.find("/"):len(url)] + e_url
+            e_host = url[0:url.find("/")]
+        if ":" in e_host:
+            h_port = e_host.split(":")[1]
+            e_host = e_host.split(":")[0]
+    if ":*" in url:
+        if "/" in url:
+            e_url = url[url.find("/"):len(url)]
+            e_host = url[0:url.find("/")]
+        else:
+            e_host = url
+
+    if ":*" not in url and "/*" not in url:
+        e_host = url
+        if "/" in url:
+            e_url = url[url.find("/"):len(url)]
+            e_host = url[0:url.find("/")]
+        if ":" in e_host:
+            h_port = e_host.split(":")[1]
+            e_host = e_host.split(":")[0]
+
+    if e_host != None:
+        if e_host[0]!= "*":
+            e_host = "^" + e_host
+        if e_host[-1] != "*":
+            e_host = e_host + "$"
+    if e_url!= None:
+        if e_url[0]!= "*":
+            e_url = "^" + e_url
+        if e_url[-1] != "*":
+            e_url = e_url + "$"
+
+    return e_host,e_url,h_port
+    #print("processUrl:+++", url)
+
+
+
